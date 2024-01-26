fb02c475ef214409e03718ee2d4060336cdfde09
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-26 16:29:49 -0500

    Initial project organization

---
+++ EnhancedTodoList./Model/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedTodoList.
+//
+//  Created by Lewis Brocklehurst on 2024-01-26.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
