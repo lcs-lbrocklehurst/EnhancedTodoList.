a274b773943109b2724d8b425003550ea55bb281
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-26 16:24:28 -0500

    Initial Commit

---
+++ EnhancedTodoList./ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnhancedTodoList.
+//
+//  Created by Lewis Brocklehurst on 2024-01-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnhancedTodoList./EnhancedTodoList_App.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoList_App.swift
+//  EnhancedTodoList.
+//
+//  Created by Lewis Brocklehurst on 2024-01-26.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoList_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
